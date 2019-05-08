e27dab0351c3edbf6f87a38842e8975091ee2c1a
Андгуладзе Нодар <pandabrouzz@gmail.com>
2019-05-09 00:04:11 +0300

    v2. Изменение внешнего вида UI и стабилизация приема данных с порта. Оформление тулл стрип панели.

--- testing/Form1.Designer.cs
+++ testing/Form1.Designer.cs
@@ -29,12 +29,167 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.on_button = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.number = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.date_time = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.current_lux_value = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.top_menuStrip = new System.Windows.Forms.MenuStrip();
+            this.COM_ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выходToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.port_label = new System.Windows.Forms.Label();
+            this.connection_label = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.top_menuStrip.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // on_button
+            // 
+            this.on_button.Location = new System.Drawing.Point(12, 31);
+            this.on_button.Name = "on_button";
+            this.on_button.Size = new System.Drawing.Size(122, 25);
+            this.on_button.TabIndex = 3;
+            this.on_button.Text = "ON/OFF";
+            this.on_button.UseVisualStyleBackColor = true;
+            this.on_button.Click += new System.EventHandler(this.on_button_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.number,
+            this.date_time,
+            this.current_lux_value});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 62);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(529, 217);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // number
+            // 
+            this.number.HeaderText = "№";
+            this.number.Name = "number";
+            this.number.ReadOnly = true;
+            // 
+            // date_time
+            // 
+            this.date_time.HeaderText = "Дата и время";
+            this.date_time.Name = "date_time";
+            this.date_time.ReadOnly = true;
+            // 
+            // current_lux_value
+            // 
+            this.current_lux_value.HeaderText = "Освещенность";
+            this.current_lux_value.Name = "current_lux_value";
+            this.current_lux_value.ReadOnly = true;
+            // 
+            // top_menuStrip
+            // 
+            this.top_menuStrip.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.top_menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.COM_ToolStripMenuItem,
+            this.выходToolStripMenuItem});
+            this.top_menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.top_menuStrip.Name = "top_menuStrip";
+            this.top_menuStrip.Size = new System.Drawing.Size(553, 28);
+            this.top_menuStrip.TabIndex = 5;
+            this.top_menuStrip.Text = "menuStrip1";
+            // 
+            // COM_ToolStripMenuItem
+            // 
+            this.COM_ToolStripMenuItem.Name = "COM_ToolStripMenuItem";
+            this.COM_ToolStripMenuItem.Size = new System.Drawing.Size(102, 24);
+            this.COM_ToolStripMenuItem.Text = "COM порты";
+            this.COM_ToolStripMenuItem.DropDownOpening += new System.EventHandler(this.COM_ToolStripMenuItem_DropDownOpening);
+            this.COM_ToolStripMenuItem.DropDownItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.COM_ToolStripMenuItem_DropDownItemClicked);
+            // 
+            // выходToolStripMenuItem
+            // 
+            this.выходToolStripMenuItem.Name = "выходToolStripMenuItem";
+            this.выходToolStripMenuItem.Size = new System.Drawing.Size(65, 24);
+            this.выходToolStripMenuItem.Text = "Выход";
+            this.выходToolStripMenuItem.Click += new System.EventHandler(this.выходToolStripMenuItem_Click);
+            // 
+            // port_label
+            // 
+            this.port_label.AutoSize = true;
+            this.port_label.Location = new System.Drawing.Point(3, 0);
+            this.port_label.Name = "port_label";
+            this.port_label.Size = new System.Drawing.Size(46, 17);
+            this.port_label.TabIndex = 6;
+            this.port_label.Text = "label1";
+            // 
+            // connection_label
+            // 
+            this.connection_label.AutoSize = true;
+            this.connection_label.Location = new System.Drawing.Point(281, 0);
+            this.connection_label.Name = "connection_label";
+            this.connection_label.Size = new System.Drawing.Size(46, 17);
+            this.connection_label.TabIndex = 7;
+            this.connection_label.Text = "label1";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.port_label, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.connection_label, 1, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 290);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(557, 24);
+            this.tableLayoutPanel1.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(553, 314);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.on_button);
+            this.Controls.Add(this.top_menuStrip);
+            this.MainMenuStrip = this.top_menuStrip;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.top_menuStrip.ResumeLayout(false);
+            this.top_menuStrip.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.Button on_button;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn number;
+        private System.Windows.Forms.DataGridViewTextBoxColumn date_time;
+        private System.Windows.Forms.DataGridViewTextBoxColumn current_lux_value;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.MenuStrip top_menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem COM_ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выходToolStripMenuItem;
+        private System.Windows.Forms.Label port_label;
+        private System.Windows.Forms.Label connection_label;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
     }
 }
 
--- testing/Form1.cs
+++ testing/Form1.cs
@@ -5,16 +5,107 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 using System.Windows.Forms;
+using System.IO.Ports;
+using System.Timers;
 
 namespace testing
 {
     public partial class Form1 : Form
     {
+        static SerialPort serialPort = new SerialPort("COM3", 9600);
+        bool isConnected = false; //флаг коннекта к плате
+        bool on_state = true;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void OnTimedEvent(object sender, EventArgs myEventArgs)
+        {
+            if (!serialPort.IsOpen) return;
+            try // так как после закрытия окна таймер еще может выполнится или предел ожидания может быть превышен
+            {
+                serialPort.DiscardInBuffer(); // удалим накопившееся в буфере
+                string strFromPort = serialPort.ReadLine(); // считаем последнее значение 
+                int rowNumber = dataGridView1.Rows.Add();                
+                dataGridView1.Rows[rowNumber].Cells[0].Value = rowNumber;
+                dataGridView1.Rows[rowNumber].Cells[1].Value = System.DateTime.Now;
+                dataGridView1.Rows[rowNumber].Cells[2].Value = strFromPort;
+                dataGridView1.CurrentCell.Selected = false;
+                dataGridView1.CurrentCell = dataGridView1.Rows[rowNumber].Cells[2];
+                dataGridView1.Rows[rowNumber].Cells[2].Selected = true;
+            }
+            catch { }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (serialPort.IsOpen)
+                serialPort.Close();
+        }
+
+        private void on_button_Click(object sender, EventArgs e)
+        {
+            if (isConnected && serialPort.IsOpen)
+            {
+                if (on_state)
+                {
+                    serialPort.Write("1");
+                    on_state = false;
+                    on_button.Text = "OFF";
+                }
+                else
+                {
+                    serialPort.Write("0");
+                    on_state = true;
+                    on_button.Text = "ON";
+                }
+            }
+            else
+            {
+                MessageBox.Show("Сначала необходимо подключиться к порту.");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            on_button.Text = "ON";
+            System.Threading.Thread.Sleep(500);
+            timer1.Tick += OnTimedEvent;
+            timer1.Interval = 1000;
+            timer1.Start();
+
+            connection_label.Text = "Порт закрыт";
+            port_label.Text = "COM порт не назначен";
+        }
+
+        private void выходToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void COM_ToolStripMenuItem_DropDownOpening(object sender, EventArgs e)
+        {
+            COM_ToolStripMenuItem.DropDown.Items.Clear();
+            string[] portnames = SerialPort.GetPortNames();
+            foreach (string portName in portnames)
+            {
+                COM_ToolStripMenuItem.DropDown.Items.Add(portName);
+            }
+        }
+
+        private void COM_ToolStripMenuItem_DropDownItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            isConnected = true;
+            string selectedPort = e.ClickedItem.Text;
+            serialPort.PortName = selectedPort;
+            serialPort.DtrEnable = true;
+            serialPort.ReadTimeout = 1000;
+            serialPort.Open();
+            connection_label.Text = "Порт открыт";
+            port_label.Text = selectedPort;
         }
     }
 }
